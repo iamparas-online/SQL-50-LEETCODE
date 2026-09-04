78a5e9b72a006df33318162c362b3af51708b13a
Paras <new043one@gmail.com>
2026-09-04 09:28:49 +0530

    Add solution: Smallest Stable Index I

--- smallest-stable-index-i/solution.sql
+++ smallest-stable-index-i/solution.sql
@@ -6,5 +6,6 @@ class Solution {
         leftmax[0]=nums[0];
 
         for(int i=1;i<=nums.length;i++){
-        leftmax[i] = maximum(leftmax[i-1], nums[i]);
+            leftmax[i] = maximum(leftmax[i-1], nums[i]);
+        }
     }
