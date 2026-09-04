38d82bbf679e34692c2c6dbc085599f38f2c8cfc
Paras <new043one@gmail.com>
2026-09-04 10:02:17 +0530

    Add solution: Smallest Stable Index I

--- smallest-stable-index-i/solution.sql
+++ smallest-stable-index-i/solution.sql
@@ -3,9 +3,9 @@
         rightmin[lastIndex] = nums[lastIndex];
 
         for(int i=1;i<nums.length;i++){
-            leftmax[i] = Mathmax(leftmax[i-1], nums[i]);
+            leftmax[i] = Math.max(leftmax[i-1], nums[i]);
         }
 
         for (int i = lastIndex - 1; i >= 0; i--) {
-            rightMin[i] = Math.min(rightMin[i + 1], nums[i]);
+            rightin[i] = Math.min(rightMin[i + 1], nums[i]);
         }
