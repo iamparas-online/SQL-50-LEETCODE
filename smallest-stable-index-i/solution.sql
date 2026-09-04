3c298d39fabefacda54f97b97a24d2f6785f7377
Paras <new043one@gmail.com>
2026-09-04 09:58:19 +0530

    Add solution: Smallest Stable Index I

--- smallest-stable-index-i/solution.sql
+++ smallest-stable-index-i/solution.sql
@@ -15,7 +15,7 @@ class Solution {
 
         rightMin[lastIndex] = nums[lastIndex];
 
-for (int i = lastIndex - 1; i >= 0; i--) {
+ for (int i = lastIndex - 1; i >= 0; i--) {
     rightMin[i] = Math.min(rightMin[i + 1], nums[i]);
 }
     }
