18af3cdc76af60f03459122acb12aef460b6299a
Paras <new043one@gmail.com>
2026-09-04 09:22:23 +0530

    Add solution: Smallest Stable Index I

--- smallest-stable-index-i/solution.sql
+++ smallest-stable-index-i/solution.sql
@@ -2,5 +2,7 @@
 class Solution {
     public int firstStableIndex(int[] nums, int k) {
         
-        int [] leftmax = new int[nums.length]; 
-    }
+        int [] leftmax = new int[nums.length];
+
+    }l
+}
