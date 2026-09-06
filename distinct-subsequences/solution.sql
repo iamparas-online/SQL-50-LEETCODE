41b228f6764e05f068784ee9748b9f3c885faa14
Paras <new043one@gmail.com>
2026-09-06 21:59:58 +0530

    Add solution: Distinct Subsequences

--- distinct-subsequences/solution.sql
+++ distinct-subsequences/solution.sql
@@ -16,7 +16,7 @@ class Solution {
 
                 if (s.charAt(i - 1) == t.charAt(j - 1)) {
 
-                    // Use s[i-1] + Skip s[i-1
+                    //
                     dp[i][j] = dp[i - 1][j - 1]
                              + dp[i - 1][j];
 
