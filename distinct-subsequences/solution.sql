3c47901d7c8fd89cad03a3b6bff23f6439eb2ded
Paras <new043one@gmail.com>
2026-09-06 21:59:37 +0530

    Add solution: Distinct Subsequences

--- distinct-subsequences/solution.sql
+++ distinct-subsequences/solution.sql
@@ -1,12 +1,24 @@
 
-                } else {
+class Solution {
+    public int numDistinct(String s, String t) {
 
-                    // Skip s[i-1]
-                    dp[i][j] = dp[i - 1][j];
-                }
-            }
+        int m = s.length();
+        int n = t.length();
+
+        int[][] dp = new int[m + 1][n + 1];
+
+        // Empty t can be made in 1 wa
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = 1;
         }
 
-        return dp[m][n];
-    }
-}
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+
+                    // Use s[i-1] + Skip s[i-1]
+                    dp[i][j] = dp[i - 1][j - 1]
+                             + dp[i - 1][j];
+
+                } else {
