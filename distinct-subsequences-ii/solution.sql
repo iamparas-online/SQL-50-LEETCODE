e3d1a867c395da72057fea37d0c609908c33c0ae
Paras <new043one@gmail.com>
2026-09-07 22:53:22 +0530

    Add solution: Distinct Subsequences II

--- distinct-subsequences-ii/solution.sql
+++ distinct-subsequences-ii/solution.sql
@@ -1,20 +1,2 @@
 
 class Solution {
-    public int distinctSubseqII(String s) {
-        long MOD = 1000000007;
-
-        long[] last = new long[26];
-        long dp = 1;
-
-        for (char c : s.toCharArray()) {
-            int i = c - 'a';
-
-            long newDp = (2 * dp - last[i] + MOD) % MOD;
-
-            last[i] = dp;
-            dp = newDp;
-        }
-
-        return (int)((dp - 1 + MOD) % MOD);
-    }
-}
