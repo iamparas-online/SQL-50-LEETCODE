d78d2d57f833161e8494bd85767f39852161f4c0
Paras <new043one@gmail.com>
2026-09-09 10:46:44 +0530

    Add solution: Count Commas in Range II

--- count-commas-in-range-ii/solution.sql
+++ count-commas-in-range-ii/solution.sql
@@ -2,8 +2,8 @@
 class Solution {
     public long countCommas(long n) {
         if(n>=10000){
-            return n-9999
+            return n-9999;
         }
-        return
+        return 0;
     }
 }
