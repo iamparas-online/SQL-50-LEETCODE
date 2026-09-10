dd72bf467590cccbc6342b7af9a8824f7f6bfb03
Paras <new043one@gmail.com>
2026-09-10 23:26:02 +0530

    Add solution: Count Nodes Equal to Average of Subtree

--- count-nodes-equal-to-average-of-subtree/solution.sql
+++ count-nodes-equal-to-average-of-subtree/solution.sql
@@ -1,12 +1,8 @@
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
+ * }
+ */
+class Solution {
+    public int averageOfSubtree(TreeNode root) {
+        
+    }
+}
