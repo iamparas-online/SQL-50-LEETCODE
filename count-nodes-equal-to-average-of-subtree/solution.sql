fbe9b2ed9dfcbbbcbba020f8cb2da37f14c40b2c
Paras <new043one@gmail.com>
2026-09-10 23:26:32 +0530

    Add solution: Count Nodes Equal to Average of Subtree

--- count-nodes-equal-to-average-of-subtree/solution.sql
+++ count-nodes-equal-to-average-of-subtree/solution.sql
@@ -1,11 +1,12 @@
 
 
-        int average = sum / count;
-
-        if (root.val == average) {
-            ans++;
-        }
-
-        return new int[]{sum, count};
+    public int averageOfSubtree(TreeNode root) {
+        dfs(root);
+        return ans;
     }
-}
+
+    // returns {sum, count}
+    public int[] dfs(TreeNode root) {
+
+        if (root == null) {
+            return new int[]{0, 0};
