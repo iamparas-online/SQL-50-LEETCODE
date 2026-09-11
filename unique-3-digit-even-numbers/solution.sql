15f238aadf30622a09cc33f232e0e98ee5da58ec
Paras <new043one@gmail.com>
2026-09-11 11:15:24 +0530

    Add solution: Unique 3-Digit Even Numbers

--- unique-3-digit-even-numbers/solution.sql
+++ unique-3-digit-even-numbers/solution.sql
@@ -1,1 +1,11 @@
 
+            for (int j = 0; j < digits.length; j++) {
+                for (int k = 0; k < digits.length; k++) {
+
+                    // Cannot use the same copy twice
+                    if (i == j || j == k || i == k)
+                        continue;
+
+                    // No leading zero
+                    if (digits[i] == 0)
+                        continue;
