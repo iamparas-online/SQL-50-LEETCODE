57fda5258af2a3b92935cc173548432433059249
Paras <new043one@gmail.com>
2026-09-11 18:13:17 +0530

    Add solution: Customer Who Visited but Did Not Make Any Transactions

--- customer-who-visited-but-did-not-make-any-transactions/solution.sql
+++ customer-who-visited-but-did-not-make-any-transactions/solution.sql
@@ -1,6 +1,6 @@
 
 # Write your MySQL query statement below
-SELECT v.customer_id, COUNT(v.visit_id) AS count_no_trans
+SELECT v.customer_id, COUNT(v.visit_id) A count_no_trans
 FROM Visits v
 LEFT JOIN Transactions t
     ON v.visit_id = t.visit_id
