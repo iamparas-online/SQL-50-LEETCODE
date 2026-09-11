4e471f916f84e3c21bc3eed346070335272e1485
Paras <new043one@gmail.com>
2026-09-11 18:22:31 +0530

    Add solution: Rising Temperature

--- rising-temperature/solution.sql
+++ rising-temperature/solution.sql
@@ -1,6 +1,6 @@
 
 # Write your MySQL query statement below
 select w1.id
-from weather w1 and weather w2 
+from weather w1 an weather w2 
 where datediff(recordDate)=1 
 and temperature.w1 > temperature.w2
