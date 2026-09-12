6db7ac53d56a01c6211b87c66812faf115813cf5
Paras <new043one@gmail.com>
2026-09-12 11:36:57 +0530

    Add solution: Maximum Score of Non-overlapping Intervals

--- maximum-score-of-non-overlapping-intervals/solution.sql
+++ maximum-score-of-non-overlapping-intervals/solution.sql
@@ -1,11 +1,2 @@
 
-        for (int i = 0; i < Math.min(a.size(), b.size()); i
-        ++) {
-            if (!a.get(i).equals(b.get(i))) {
-                return a.get(i) < b.get(i);
-            }
-        }
-
-        return a.size() < b.size();
-    }
-}
+ 
