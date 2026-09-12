cc9f01c2ce1ae59186e921a8684bac0ba80247de
Paras <new043one@gmail.com>
2026-09-12 11:34:49 +0530

    Add solution: Maximum Score of Non-overlapping Intervals

--- maximum-score-of-non-overlapping-intervals/solution.sql
+++ maximum-score-of-non-overlapping-intervals/solution.sql
@@ -1,7 +1,12 @@
 
-class Solution {
-    public int[] maximumWeight(List<List<Integer>> intervals) {
-        
-    }
-}
-}
+
+    public int[] maximumWeight(List<List<Integer>> intervalsList) {
+        n = intervalsList.size();
+
+        intervals = new int[n][4];
+
+        for (int i = 0; i < n; i++) {
+            intervals[i][0] = intervalsList.get(i).get(0);
+            intervals[i][1] = intervalsList.get(i).get(1);
+            intervals[i][2] = intervalsList.get(i).get(2);
+            intervals[i][3] = i;
