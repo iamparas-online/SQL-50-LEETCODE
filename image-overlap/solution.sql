c3b9897d4618c596b3eebe69d44bf1c34f0d1d38
Paras <new043one@gmail.com>
2026-09-13 11:14:13 +0530

    Add solution: Image Overlap

--- image-overlap/solution.sql
+++ image-overlap/solution.sql
@@ -1,2 +1,25 @@
 
-c
+
+        for (int dr = -n + 1; dr < n; dr++) {
+            for (int dc = -n + 1; dc < n; dc++) {
+
+                int count = 0;
+
+                for (int i = 0; i < n; i++) {
+                    for (int j = 0; j < n; j++) {
+
+                        int x = i + dr;
+                        int y = j + dc;
+
+                        if (x >= 0 && x < n && y >= 0 && y < n) {
+                            if (img1[i][j] == 1 && img2[x][y] == 
+                            1) {
+                                count++;
+                            }
+                        }
+                    }
+                }
+
+                ans = Math.max(ans, count);
+            }
+        }
