f2384d5b562e646ef5ca790522ac76d34e7b075f
Paras <new043one@gmail.com>
2026-09-15 17:04:34 +0530

    Add solution: Maximum Number of Non-overlapping Palindrome Substrings

--- maximum-number-of-non-overlapping-palindrome-substrings/solution.sql
+++ maximum-number-of-non-overlapping-palindrome-substrings/solution.sql
@@ -1,23 +1,25 @@
 
+``java
+class Solution {
+    public int maxPalindromes(String s, int k) {
+        int n = s.length();
 
-        // dp[i] = maximum palindromes using first i characters
-        int[] dp = new int[n + 1];
+        // palindrome[i][j] = true if s[i...j] is a palindrome
+        boolean[][] palindrome = new boolean[n][n];
 
-        for (int i = 1; i <= n; i++) {
-            // Don't use a palindrome ending here
-            dp[i] = dp[i - 1];
+        // Check all substrings
+        for (int len = 1; len <= n; len++) {
+            for (int i = 0; i + len <= n; i++) {
+                int j = i + len - 1;
 
-            // Try every possible starting point
-            for (int j = 0; j < i; j++) {
-
-                // length must be at least k
-                if (i - j >= k && palindrome[j][i - 1]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                if (s.charAt(i) == s.charAt(j)) {
+                    if (len <= 2) {
+                        palindrome[i][j] = true;
+                    } else {
+                        palindrome[i][j] = palindrome[i + 1][j - 1];
+                    }
                 }
             }
         }
 
-        return dp[n];
-    }
-}
-
+        // dp[i] = maximum palindromes using first i characters
