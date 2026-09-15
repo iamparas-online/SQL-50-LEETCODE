e9b3c32b16226d75e4bca55ac5d45c05c071f4e3
Paras <new043one@gmail.com>
2026-09-15 17:04:23 +0530

    Add solution: Maximum Number of Non-overlapping Palindrome Substrings

--- maximum-number-of-non-overlapping-palindrome-substrings/solution.sql
+++ maximum-number-of-non-overlapping-palindrome-substrings/solution.sql
@@ -1,4 +1,22 @@
 
-class Solution {
-    public int maxPalindromes(String s, int k) {
-        
+
+        // dp[i] = maximum palindromes using first i characters
+        int[] dp = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            // Don't use a palindrome ending here
+            dp[i] = dp[i - 1];
+
+            // Try every possible starting point
+            for (int j = 0; j < i; j++) {
+
+                // length must be at least k
+                if (i - j >= k && palindrome[j][i - 1]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        return dp[n];
+    }
+}
