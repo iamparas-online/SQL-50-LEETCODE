3095b7fda524698c2c282ec7ac9bc6fed1a21e85
Paras <new043one@gmail.com>
2026-09-18 17:19:57 +0530

    Add solution: First Bad Version

--- first-bad-version/solution.sql
+++ first-bad-version/solution.sql
@@ -8,7 +8,8 @@ class Solution {
         int high = n;
 
         while (low < high) {
-            int mid = (low + high)  2;
+            int mid = (low + high) / 2;
 
             if (isBadVersion(mid)) {
                 high = mid;
+            } else {
