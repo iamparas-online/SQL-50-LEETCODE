964a0cb5109307c6621072d6dbc87cf2ad8d2d54
Paras <new043one@gmail.com>
2026-09-18 17:09:05 +0530

    Add solution: Maximum Number of Non-Overlapping Substrings

--- maximum-number-of-non-overlapping-substrings/solution.sql
+++ maximum-number-of-non-overlapping-substrings/solution.sql
@@ -1,5 +1,4 @@
 
-class Solution {
     public List<String> maxNumOfSubstrings(String s) {
         int n = s.length();
 
@@ -10,3 +9,17 @@ class Solution {
 
         // Find first and last occurrence
         for (int i = 0; i < n; i++) {
+            int c = s.charAt(i) - 'a';
+            first[c] = Math.min(first[c], i);
+            last[c] = i;
+        }
+
+        List<int[]> intervals = new ArrayList<>();
+
+        // Build valid intervals
+        for (int c = 0; c < 26; c++) {
+            if (last[c] == -1) continue;
+
+            int l = first[c];
+            int r = last[c];
+            boolean valid = true;
