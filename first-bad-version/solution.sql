bd316c6b5b9949d59231609b015128bf345f48ac
Paras <new043one@gmail.com>
2026-09-18 17:17:45 +0530

    Add solution: First Bad Version

--- first-bad-version/solution.sql
+++ first-bad-version/solution.sql
@@ -1,12 +1,13 @@
 
+// The isBadVersion API is defined for you.
+// boolean isBadVersion(int version);
+
+class Solution {
+    public int firstBadVersion(int n) {
+
+        int low = 1;
+        int high = n;
 
         while (low < high) {
 
             int mid = low + (high - low) / 2;
-
-            if (isBadVersion(mid)) {
-                high = mid;
-            } else {
-                low = mid + 1;
-            }
-        }
