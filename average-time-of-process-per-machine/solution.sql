7dd3219a6a8b41b37c2d62cde09b8240bdf6bfb3
Paras <new043one@gmail.com>
2026-09-19 17:53:31 +0530

    Add solution: Average Time of Process per Machine

--- average-time-of-process-per-machine/solution.sql
+++ average-time-of-process-per-machine/solution.sql
@@ -6,4 +6,6 @@ FROM Activity a1
 INNER JOIN Activity a2
 ON a1.process_id = a2.process_id
 AND a1.machine_id = a2.machine_id
-AND a1.timestamp='start' < a2.timestamp='end'
+a1.activity_type = 'start'
+AND a2.activity_type = 'end'
+GROUP BY a1.machine_id;
