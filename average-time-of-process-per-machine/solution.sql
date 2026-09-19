bd23cc89d91dd20e8e0932855cb4ece9ea4f4e67
Paras <new043one@gmail.com>
2026-09-19 17:50:25 +0530

    Add solution: Average Time of Process per Machine

--- average-time-of-process-per-machine/solution.sql
+++ average-time-of-process-per-machine/solution.sql
@@ -1,6 +1,6 @@
 
 # Write your MySQL query statement below
-SELECT a1.machine_id,
+SELECT a1.machine_id,ROUND(AVG(a2.timestamp - a1.timestamp), 3) AS 
 FROM Activity a1
 INNER JOIN Activity a2
 ON a1.process_id = a2.process_id
