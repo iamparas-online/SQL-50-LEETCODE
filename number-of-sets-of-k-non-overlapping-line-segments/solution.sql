6072874c9208ec2da62ec5ff5530d67d6d0c2f7b
Paras <new043one@gmail.com>
2026-09-19 17:39:34 +0530

    Add solution: Number of Sets of K Non-Overlapping Line Segments

--- number-of-sets-of-k-non-overlapping-line-segments/solution.sql
+++ number-of-sets-of-k-non-overlapping-line-segments/solution.sql
@@ -1,14 +1,1 @@
 
-class Solution {
-
-
-   //Bhai ye kya question h aarush bhai agar tu dakh raha h to samjha dena 
-
-    static final long MOD = 1000000007;
-
-    public int numberOfSets(int n, int k) {
-        int N = n + k - 1;
-        int R = 2 * k;
-
-        long[] dp = new long[R + 1];
-        dp[0] = 1;
