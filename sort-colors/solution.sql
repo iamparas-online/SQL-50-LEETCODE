4f83567762ab1c1437abd4e1912e59c7c5d28f1e
Paras <new043one@gmail.com>
2026-09-20 12:26:36 +0530

    Add solution: Sort Colors

--- sort-colors/solution.sql
+++ sort-colors/solution.sql
@@ -9,17 +9,3 @@ class Solution {
 
         while(j<=k){
             if(nums[j]==1){
-                j++;
-            }
-            else if(nums[j]==2){
-                swap(nums[j],nums[k]);
-                k--;
-            }
-            else{
-                swap(nu[j],nums[i]);
-                i++;
-                j++;
-            }
-        }
-    }
-}
