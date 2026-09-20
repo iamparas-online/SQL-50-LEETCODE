eafb15f98434b9b72c4d1dc6d3160a7e2cca33c7
Paras <new043one@gmail.com>
2026-09-20 18:00:30 +0530

    Add solution: Ransom Note

--- ransom-note/solution.sql
+++ ransom-note/solution.sql
@@ -1,12 +1,13 @@
 
+        int [] count = new int [26];
+
+        for(int i=0;i<m.length();i++){
+            char c = m.charAt(i);
+            count[c - 'a']++;
+        }
 
         for(int i=0;i<r.length();i++){
             
 
             if(count[c - 'a'] == 0){
                 return false;
-            }
-            count[c - 'a']--;  
-        }
-        return true ;
-    }
