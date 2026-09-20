bd87ba816b11bb67288d224055b123dc7a9471d8
Paras <new043one@gmail.com>
2026-09-20 12:30:26 +0530

    Add solution: Sort Colors

--- sort-colors/solution.sql
+++ sort-colors/solution.sql
@@ -1,19 +1,20 @@
 
+class Solution {
+    public void sortColors(int[] nums) {
+        
+        int n=nums.length;
+        int i=0;    //0
+        int j=0;    //1
+        int k=n-1;  //2
+
+        while(j<=k){
+            if(nums[j]==1){
+                j++;
+            }
+            else if(nums[j]==2){
+
                 int temp = nums[j];
                 nums[j]=nums[k];
                 nums[k]=temp;
 
                 k--;
-            }
-            else{
-
-                int temp = nums[j];
-                nums[j]=nums[i];
-                nums[]=temp;
-
-                i++;
-                j++;
-            }
-        }
-    }
-}
