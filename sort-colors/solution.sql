d6dbcf5ab93ed7fcc3d3e279ce04c642a43df9c4
Paras <new043one@gmail.com>
2026-09-20 12:23:10 +0530

    Add solution: Sort Colors

--- sort-colors/solution.sql
+++ sort-colors/solution.sql
@@ -15,8 +15,7 @@ class Solution {
             else if(n[j]==1){
                 swap(n[j],n[i]);
                 i++;
-                j+
+                j++;
             }
         }
     }
-}
