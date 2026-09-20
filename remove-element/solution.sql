c3e1cd137d1a7d82122bae2674889e6b1608b481
Paras <new043one@gmail.com>
2026-09-20 11:56:16 +0530

    Add solution: Remove Element

--- remove-element/solution.sql
+++ remove-element/solution.sql
@@ -5,7 +5,7 @@ class Solution {
         int n=nums.length;
 
         for(int i=0;i<n;i++){
-            if(nums[i] != ){
+            if(nums[i] != va){
                 nums[k]=nums[i];
                 k++;
             }
