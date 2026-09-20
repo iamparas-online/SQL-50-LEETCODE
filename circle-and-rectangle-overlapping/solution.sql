6540ee70aa8b19b97cabd7b65610a44d4d1afd95
Paras <new043one@gmail.com>
2026-09-20 11:25:03 +0530

    Add solution: Circle and Rectangle Overlapping

--- circle-and-rectangle-overlapping/solution.sql
+++ circle-and-rectangle-overlapping/solution.sql
@@ -1,10 +1,12 @@
 
 class Solution {
-    public boolean checkOverlap(int radius, 
-    int xCenter, int yCenter,
-                                int x1, int 
-                                y1, int x2, 
-                                int y2) {
+    public boolean checkOverlap(int radius, int xCenter, int yCenter,
+                                int x1, int y1, int x2, int y2) {
 
         int xi;
         int yi;
+
+        // nearest x
+        if (x1 > xCenter) {
+            xi = x1;
+        } else if (x2 < xCenter) {
