bac073907486274d1a0ecfcb4f0aa8ea8c53acbe
Paras <new043one@gmail.com>
2026-09-20 17:20:23 +0530

    Add solution: First Bad Version

--- first-bad-version/solution.sql
+++ first-bad-version/solution.sql
@@ -1,15 +1,1 @@
 
-
-        while (low < high) {
-            int mid = low + (high - low) / 2;
-
-            if (isBadVersion(mid)) {
-                high = mid;
-            } else {
-                low = mid + 1;
-            }
-        }
-
-        return low;
-    }
-}
