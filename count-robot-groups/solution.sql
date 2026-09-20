0cdcafe5eb517a3eb28654a025986740be0ce8b7
Paras <new043one@gmail.com>
2026-09-20 18:45:22 +0530

    Add solution: Count Robot Groups

--- count-robot-groups/solution.sql
+++ count-robot-groups/solution.sql
@@ -10,7 +10,7 @@ class Solution {
             if(position[i+1] - position[i] <= distance){
                 continue;
             }
-            else if() {}
+            else if(spe) {}
         }
 
 
