556ae8e3a216662d1243f37e6f5a47d2f72b337c
Paras <new043one@gmail.com>
2026-09-20 18:40:25 +0530

    Add solution: Count Robot Groups

--- count-robot-groups/solution.sql
+++ count-robot-groups/solution.sql
@@ -3,7 +3,7 @@ class Solution {
     public int countGroups(int[] position, int[] speed, int distance) {
         int n = position.length;
         int group = 1;
-        int 
+        int group_
 
     }
 }
