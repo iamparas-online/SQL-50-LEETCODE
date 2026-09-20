1f389e8b128b54e06702949bada42a9cc50f6688
Paras <new043one@gmail.com>
2026-09-20 17:48:26 +0530

    Add solution: Ransom Note

--- ransom-note/solution.sql
+++ ransom-note/solution.sql
@@ -1,9 +1,8 @@
 
 class Solution {
-    public boolean canConstruct(String r, String magazin) {
+    public boolean canConstruct(String r, String m) {
         int [] count = new int [26];
 
 
         for(int i=0;i<)
     }
-}
