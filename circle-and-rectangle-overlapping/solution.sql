5c8be74d33dfd27fb554326098253b7a7f7cc52e
Paras <new043one@gmail.com>
2026-09-20 11:25:04 +0530

    Add solution: Circle and Rectangle Overlapping

--- circle-and-rectangle-overlapping/solution.sql
+++ circle-and-rectangle-overlapping/solution.sql
@@ -10,3 +10,16 @@ class Solution {
         if (x1 > xCenter) {
             xi = x1;
         } else if (x2 < xCenter) {
+            xi = x2;
+        } else {
+            xi = xCenter;
+        }
+
+        // nearest y
+        if (y1 > yCenter) {
+            yi = y1;
+        } else if (y2 < yCenter) {
+            yi = y2;
+        } else {
+            yi = yCenter;
+        }
