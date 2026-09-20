9eb8df9da4e8f5e0777b777ffb1ef9f984fb0104
Paras <new043one@gmail.com>
2026-09-20 18:46:39 +0530

    Add solution: Count Robot Groups

--- count-robot-groups/solution.sql
+++ count-robot-groups/solution.sql
@@ -12,7 +12,7 @@ class Solution {
             }
             else if(speed[i] <= group_speed){
                 group++;
-                group_speed=s
+                group_speed=speed[]
             }
         }
 
