ed9e4343f8ef8f39c7c254e2f74641b4b227e4c7
Paras <new043one@gmail.com>
2026-09-20 11:41:50 +0530

    Add solution: Reverse Degree of a String

--- reverse-degree-of-a-string/solution.sql
+++ reverse-degree-of-a-string/solution.sql
@@ -5,7 +5,7 @@ class Solution {
         int result=0;
 
         for(int i=0;i<n;i++){
-            result = ('z'-s[]+1) *(i+1); 
+            result += ('z'-s[]+1) *(i+1); 
         }
 
         return result;
