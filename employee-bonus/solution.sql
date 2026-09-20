43a8e38347d09e62ea7de15e6ed0d3516d657d2c
Paras <new043one@gmail.com>
2026-09-20 23:46:55 +0530

    Add solution: Employee Bonus

--- employee-bonus/solution.sql
+++ employee-bonus/solution.sql
@@ -1,7 +1,7 @@
 
 # Write your MySQL query statement below
-SELECT e.name ,b.onus 
+SELECT e.name ,b.bonus 
 FROM Employee e
-LEFT JOIN Bonus b
+LEFT JOIN onus b
 ON e.empID = b.empID
 WHERE b.Bonus is NULL OR e.Bonus < 1000 
