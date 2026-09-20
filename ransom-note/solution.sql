8c01e9934c1f1f5c5ce6aa413d1838bd5bb3f280
Paras <new043one@gmail.com>
2026-09-20 17:59:58 +0530

    Add solution: Ransom Note

--- ransom-note/solution.sql
+++ ransom-note/solution.sql
@@ -1,12 +1,12 @@
 
-class Solution {
-    public boolean canConstruct(String r, String m) {
-        int [] count = new int [26];
-
-        for(int i=0;i<m.length();i++){
             char c = m.charAt(i);
             count[c - 'a']++;
         }
 
         for(int i=0;i<r.length();i++){
             
+
+            if(count[c - 'a'] == 0){
+                return false;
+            }
+            count[c - 'a']--;  
