a3feb2cbfcaa6ca5c7a4d5910ffc163b0f2dc412
Paras <new043one@gmail.com>
2026-09-20 18:00:39 +0530

    Add solution: Ransom Note

--- ransom-note/solution.sql
+++ ransom-note/solution.sql
@@ -4,8 +4,9 @@
         }
 
         for(int i=0;i<r.length();i++){
-            
+            i
             if(count[c - 'a'] == 0){
                 return false;
             }
             count[c - 'a']--;  
+        }
