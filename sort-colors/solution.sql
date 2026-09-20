9ff1feebac366976e1a48c627892ff21b2e67163
Paras <new043one@gmail.com>
2026-09-20 12:28:13 +0530

    Add solution: Sort Colors

--- sort-colors/solution.sql
+++ sort-colors/solution.sql
@@ -1,11 +1,8 @@
 
-            else if(nums[j]==2){
-                swap(nums[j],nums[k]);
-                k--;
-            }
-            else{
                 swap(nums[j],nums[i]);
                 i++;
                 j++;
             }
         }
+    }
+}
