6d760ad3ab9502e99a63e225398b6ba4607ef5b0
Paras <new043one@gmail.com>
2026-09-20 11:55:43 +0530

    Add solution: Remove Element

--- remove-element/solution.sql
+++ remove-element/solution.sql
@@ -2,7 +2,7 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
         int k=0;
-        int n=nums.length();
+        int n=nums.length(;
 
         for(int i=0;i<n;i++){
             if(nums[val] != nums[i]){
@@ -10,4 +10,3 @@ class Solution {
                 k++;
             }
         }
-        return k;
