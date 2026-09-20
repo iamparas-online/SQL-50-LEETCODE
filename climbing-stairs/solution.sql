13ea08cf83bfb76defd992e5af161d647a645f5a
Paras <new043one@gmail.com>
2026-09-20 18:37:40 +0530

    Add solution: Climbing Stairs

--- climbing-stairs/solution.sql
+++ climbing-stairs/solution.sql
@@ -1,11 +1,25 @@
 
-import java.util.Arrays;
-
-class Solution {
-
-    int[] dp = new int[46];
-
     public int solve(int n) {
 
         if (n < 0)
             return 0;
+
+        if (n == 0)
+            return 1;
+
+        if (dp[n] != -1)
+            return dp[n];
+
+        int oneStep = solve(n - 1);
+        int twoStep = solve(n - 2);
+
+        return dp[n] = oneStep + twoStep;
+    }
+
+    public int climbStairs(int n) {
+
+        Arrays.fill(dp, -1);
+
+        return solve(n);
+    }
+}
