28823fe245b93debdfa45de4d61b2e5c968b10fb
Paras <new043one@gmail.com>
2026-09-20 12:24:42 +0530

    Add solution: Sort Colors

--- sort-colors/solution.sql
+++ sort-colors/solution.sql
@@ -14,7 +14,7 @@ class Solution {
             else if(n[j]==2){
                 swap(n[j],n[i]);
                 i++;
-                j++;
+                j+
             }
             else{
                 
