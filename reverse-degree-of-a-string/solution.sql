49d5a2e8ab42a8dafdb4f89ec0c2f43143e430aa
Paras <new043one@gmail.com>
2026-09-20 11:39:09 +0530

    Add solution: Reverse Degree of a String

--- reverse-degree-of-a-string/solution.sql
+++ reverse-degree-of-a-string/solution.sql
@@ -2,6 +2,10 @@
 class Solution {
     public int reverseDegree(String s) {
         int n=s.length();
-        int result
+        int result=0;
+
+        for(int i=0;i<n;i++){
+            result = ('z's[i]+)
+        }
     }
 }
