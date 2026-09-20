e91d8f33e890772d8d9de33f864261f551502725
Paras <new043one@gmail.com>
2026-09-20 11:50:09 +0530

    Add solution: Remove Element

--- remove-element/solution.sql
+++ remove-element/solution.sql
@@ -1,4 +1,7 @@
 
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
         int k = 0;
 
         for (int i = 0; i < nums.size(); i++) {
@@ -10,4 +13,3 @@
 
         return k;        
     }
-};
