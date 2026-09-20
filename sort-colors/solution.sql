f9e8739ebc42fbc78d6f8452083026e3a7a2003a
Paras <new043one@gmail.com>
2026-09-20 12:28:32 +0530

    Add solution: Sort Colors

--- sort-colors/solution.sql
+++ sort-colors/solution.sql
@@ -10,9 +10,10 @@
                 j++;
             }
             else if(nums[j]==2){
+
                 swap(nums[j],nums[k]);
+                
                 k--;
             }
             else{
                 swap(nums[j],nums[i]);
-                i++;
