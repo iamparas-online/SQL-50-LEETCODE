5318150eb6b0b4d9f2af7dca050972dd3a880063
Paras <new043one@gmail.com>
2026-09-21 11:29:54 +0530

    Add solution: Find X Value of Array I

--- find-x-value-of-array-i/solution.sql
+++ find-x-value-of-array-i/solution.sql
@@ -1,13 +1,4 @@
 
-class Solution {
-    public long[] resultArray(int[] nums, int k) {
-
-        long[] prev = new long[k];
-        long[] result = new long[k];
-
-        for (long num : nums) {
-
-            long[] current = new long[k];
 
             int rem = (int) (num % k);
             current[rem]++;
@@ -20,3 +11,12 @@ class Solution {
                     current[newRem] += prev[r];
                 }
             }
+
+            // Move current to previous
+            prev = current;
+
+            // Add current counts to final answer
+            for (int r = 0; r < k; r++) {
+                result[r] += current[r];
+            }
+        }
