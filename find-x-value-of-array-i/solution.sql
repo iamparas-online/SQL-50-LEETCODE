442868702e84b9cfade39301249b3e53842a3b1c
Paras <new043one@gmail.com>
2026-09-21 11:11:42 +0530

    Add solution: Find X Value of Array I

--- find-x-value-of-array-i/solution.sql
+++ find-x-value-of-array-i/solution.sql
@@ -1,6 +1,11 @@
 
-class Solution {
-    public long[] resultArray(int[] nums, int k) {
-        
-    }
-}
+        for (int num : nums) {
+
+            long[] current = new long[k];
+
+            // Start a new subarray with only num
+            int remainder = num % k;
+            current[remainder]++;
+
+            // Add num to all previous subarrays
+            for (int r = 0; r < k; r++) {
