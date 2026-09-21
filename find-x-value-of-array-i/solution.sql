f5ec8198864f7c659cb42445e01a12e2b5ec4c21
Paras <new043one@gmail.com>
2026-09-21 11:11:45 +0530

    Add solution: Find X Value of Array I

--- find-x-value-of-array-i/solution.sql
+++ find-x-value-of-array-i/solution.sql
@@ -1,11 +1,12 @@
 
-        for (int num : nums) {
-
-            long[] current = new long[k];
-
-            // Start a new subarray with only num
             int remainder = num % k;
             current[remainder]++;
 
             // Add num to all previous subarrays
             for (int r = 0; r < k; r++) {
+
+                if (prev[r] > 0) {
+                    int newRemainder = (r * num) % k;
+                    current[newRemainder] += prev[r];
+                }
+            }
