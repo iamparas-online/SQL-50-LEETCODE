e5631b84757ad4e807aba8d7bdfa5d9f89397c2b
Paras <new043one@gmail.com>
2026-09-22 18:25:38 +0530

    Add solution: Find X Value of Array II

--- find-x-value-of-array-ii/solution.sql
+++ find-x-value-of-array-ii/solution.sql
@@ -1,10 +1,1 @@
 
-            int start = queries[i][2];
-            int x = queries[i][3];
-
-            update(1, 0, n - 1, index, value);
-
-            Node node = query(
-                1, 0, n - 1,
-                start, n - 1
-            );
