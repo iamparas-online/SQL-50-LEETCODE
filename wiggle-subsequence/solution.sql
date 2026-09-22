88235aa04af6f6ec004fc41751af2684968edc5b
Paras <new043one@gmail.com>
2026-09-23 00:12:33 +0530

    Add solution: Wiggle Subsequence

--- wiggle-subsequence/solution.sql
+++ wiggle-subsequence/solution.sql
@@ -9,7 +9,7 @@ class Solution {
         }
     
 
-        for(int i=0; i<nums.length-1; i++){
+        for(int i=; i<nums.length-1; i++){
             
             if(nums[i]>nums[i-1]){
                 up=down-1;
