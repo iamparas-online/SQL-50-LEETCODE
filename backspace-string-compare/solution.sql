10a5f4dfa13205af4503a6dbf53eb1b2ea31d21c
Paras <new043one@gmail.com>
2026-09-22 23:37:41 +0530

    Add solution: Backspace String Compare

--- backspace-string-compare/solution.sql
+++ backspace-string-compare/solution.sql
@@ -4,7 +4,7 @@ class Solution {
         Stack <character> First = new Stack<>();
         Stack <character> Second = new Stack<>();
 
-        for(int i=0; i)
+        for(int i=0; i<=s)
 
     }
 }
