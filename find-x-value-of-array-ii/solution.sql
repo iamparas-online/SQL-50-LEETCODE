26f36881729d3f191e86c9aa1eda3a07f1f462cc
Paras <new043one@gmail.com>
2026-09-22 18:25:41 +0530

    Add solution: Find X Value of Array II

--- find-x-value-of-array-ii/solution.sql
+++ find-x-value-of-array-ii/solution.sql
@@ -1,1 +1,22 @@
 
+        }
+    }
+
+    Node merge(Node a, Node b) {
+
+        if (a == null) return b;
+        if (b == null) return a;
+
+        Node c = new Node();
+
+        // Product of entire segment
+        c.product = (a.product * b.product) % k;
+
+        // Prefixes
+        // Prefix completely inside left
+        for (int r = 0; r < k; r++) {
+            c.pre[r] = a.pre[r];
+        }
+
+        // Prefix = entire left + prefix of right
+        for (int r = 0; r < k; r++) {
