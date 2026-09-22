448761f47cc8300b03e49955cc34c34480a9666d
Paras <new043one@gmail.com>
2026-09-22 18:22:44 +0530

    Add solution: Find X Value of Array II

--- find-x-value-of-array-ii/solution.sql
+++ find-x-value-of-array-ii/solution.sql
@@ -1,6 +1,10 @@
 
-class Solution {
-    public int[] resultArray(int[] nums, int k, int[][] queries) {
-        
-    }
-}
+            int value = queries[i][1];
+            int start = queries[i][2];
+            int x = queries[i][3];
+
+            update(1, 0, n - 1, index, value);
+
+            Node node = query(
+                1, 0, n - 1,
+                start, n - 1
