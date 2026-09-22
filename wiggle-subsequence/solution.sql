0f6706d2c13b3c5aef4b194634b7b273506c392a
Paras <new043one@gmail.com>
2026-09-23 00:11:08 +0530

    Add solution: Wiggle Subsequence

--- wiggle-subsequence/solution.sql
+++ wiggle-subsequence/solution.sql
@@ -2,21 +2,8 @@
     public int wiggleMaxLength(int[] nums) {
         int up=1;
         int down=1;
-     /*   
+     /*  
         if(nums.length<2){
             return nums.length;
         }
     */
-
-        for(int i=0; i<nums.length-1; i++){
-            
-            if(nums[i]<nums[i-1]){
-                up=down-1;
-            }
-            else{
-                down=up+1;
-            }
-        }
-
-        return Matmax(up,down);
-    }
