59e7544a877f6c54361163d29155283ccb275cfc
Paras <new043one@gmail.com>
2026-09-22 23:45:02 +0530

    Add solution: Backspace String Compare

--- backspace-string-compare/solution.sql
+++ backspace-string-compare/solution.sql
@@ -1,4 +1,16 @@
 
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        Stack <Character> First = new Stack<>();
+        Stack <Character> Second = new Stack<>();
+
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i)!='#'){
+                Fipush(First.charAt(i));
+            }
+            else{
+                pop(First.charAt(i));
+            }
         }
 
         for(int i=0; i<t.length(); i++){
@@ -8,4 +20,3 @@
             else{
                 pop(Second.charAt(i));
             }
-        }
