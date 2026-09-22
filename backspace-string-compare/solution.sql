7dd1325d01f40ea92c2e54411de3b2a3e4910956
Paras <new043one@gmail.com>
2026-09-22 23:44:39 +0530

    Add solution: Backspace String Compare

--- backspace-string-compare/solution.sql
+++ backspace-string-compare/solution.sql
@@ -1,7 +1,11 @@
 
-            }
         }
 
-        return First.equals(Second);
-    }
-}
+        for(int i=0; i<t.length(); i++){
+            if(s.charAt(i)!='#'){
+                push(Second.charAt(i));
+            }
+            else{
+                pop(Second.charAt(i));
+            }
+        }
