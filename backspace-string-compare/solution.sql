517f5df15c1b638532391264834a9a1d7127e7c1
Paras <new043one@gmail.com>
2026-09-22 23:37:09 +0530

    Add solution: Backspace String Compare

--- backspace-string-compare/solution.sql
+++ backspace-string-compare/solution.sql
@@ -1,6 +1,8 @@
 
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-        Stack<charac
+        Stack <character> First = new Stack<>();
+        Stack <character> Seco = new Stack<>();
+
     }
 }
