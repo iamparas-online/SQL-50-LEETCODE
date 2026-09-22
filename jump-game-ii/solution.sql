8c4d265e6de67bb48be20b4e0eab503e996c0e64
Paras <new043one@gmail.com>
2026-09-22 22:50:29 +0530

    Add solution: Jump Game II

--- jump-game-ii/solution.sql
+++ jump-game-ii/solution.sql
@@ -1,10 +1,9 @@
 
-        int jump=0;
-
-        for(int i=0; i<nums.lenght-1; i++){
-
             far=Math.max(far,i+nums[i]);
 
             if(i==end){
                 end=far;
                 jump++;
+            }
+        }
+        return jump;
