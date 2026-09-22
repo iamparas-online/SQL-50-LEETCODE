ebd321663d00486a0cd4a7671207ddcb43140806
Paras <new043one@gmail.com>
2026-09-23 00:13:37 +0530

    Add solution: Wiggle Subsequence

--- wiggle-subsequence/solution.sql
+++ wiggle-subsequence/solution.sql
@@ -14,7 +14,9 @@ class Solution {
             if(nums[i]>nums[i-1]){
                 up=down+1;
             }
-            else if(nums[i]<nums{}){
+            else if(nums[i]<nums[i-1]){
                 down=up-1;
             }
         }
+
+        return Math.max(up,down);
