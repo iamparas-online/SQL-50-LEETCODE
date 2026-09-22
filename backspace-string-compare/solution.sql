c0bb23e7602cbd3477fac22cd1abc68dfcdbcbe8
Paras <new043one@gmail.com>
2026-09-22 23:43:12 +0530

    Add solution: Backspace String Compare

--- backspace-string-compare/solution.sql
+++ backspace-string-compare/solution.sql
@@ -1,10 +1,11 @@
 
+    public boolean backspaceCompare(String s, String t) {
+        Stack <Character> First = new Stack<>();
+        Stack <Character> Second = new Stack<>();
 
-        for(int i=0; i<s.length; i++){
+        for(int i=0; i<s.length(); i++){
             if(s.charAt(i)!=#){
                 push(First.charAt(i));
             }
             else{
                 pop(First.charAt(i));
-            }
-        }
