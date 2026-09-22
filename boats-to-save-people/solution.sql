73b95af2d38578357112f219972b91654c6d7c5e
Paras <new043one@gmail.com>
2026-09-22 22:15:37 +0530

    Add solution: Boats to Save People

--- boats-to-save-people/solution.sql
+++ boats-to-save-people/solution.sql
@@ -7,9 +7,9 @@ class Solution {
         int right = people.length-1;
 
         while(left<=right){
-            if(people[left]+people[right]<=limit)
+            if{(people[left]+people[right]<=limit)
             left++;
-            
+        }
         right--;
         count++;
         }
