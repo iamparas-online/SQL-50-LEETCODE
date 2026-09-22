bc41d4fa4c58a646496bfda4f3cd5efb190ef68c
Paras <new043one@gmail.com>
2026-09-22 23:42:32 +0530

    Add solution: Backspace String Compare

--- backspace-string-compare/solution.sql
+++ backspace-string-compare/solution.sql
@@ -8,3 +8,15 @@
                 pop(First.charAt(i));
             }
         }
+
+        for(int i=0; i<=t.length; i++){
+            if(s.charAt(i)!=#){
+                push(Second.charAt(i));
+            }
+            else{
+                pop(Second.charAt(i));
+            }
+        }
+
+        return First.equals(Second)
+    }
