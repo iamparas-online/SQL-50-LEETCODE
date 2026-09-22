39b8f688b04909727fd0ebff7143925b80c1f78f
Paras <new043one@gmail.com>
2026-09-22 11:14:34 +0530

    Add solution: Find X Value of Array I

--- find-x-value-of-array-i/solution.sql
+++ find-x-value-of-array-i/solution.sql
@@ -1,19 +1,1 @@
 
-                if (prev[r] > 0) {
-                    int newRem = (int) ((r * num) % k);
-                    current[newRem] += prev[r];
-                }
-            }
-
-            // Move current to previous
-            prev = current;
-
-            // Add current counts to final answer
-            for (int r = 0; r < k; r++) {
-                result[r] += current[r];
-            }
-        }
-
-        return result;
-    }
-}
