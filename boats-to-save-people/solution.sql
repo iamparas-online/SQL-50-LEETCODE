bed84e7d91b9caba6d1e287ebe86e6f3d5f64d74
Paras <new043one@gmail.com>
2026-09-22 22:14:07 +0530

    Add solution: Boats to Save People

--- boats-to-save-people/solution.sql
+++ boats-to-save-people/solution.sql
@@ -2,7 +2,7 @@
 class Solution {
     public int numRescueBoats(int[] people, int limit) {
         Arrays.sort(people);
-        int count =0;
+        int count = 0;
         int left = 0;
         int right = people.length-1;
 
@@ -14,4 +14,3 @@ class Solution {
 
 
     }
-}
