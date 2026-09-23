de8308ecbeddc1c171ec978c3a2e0223fd789fd2
Paras <new043one@gmail.com>
2026-09-23 14:51:58 +0530

    Add solution: 132 Pattern

--- 132-pattern/solution.sql
+++ 132-pattern/solution.sql
@@ -1,11 +1,10 @@
 
+
+            if(nums[i]<min){
+                return true; 
             }
 
             while( !s.empty() && s.peek()<nums[i] ){
                 min = Math.max(min,s.pop()); 
 
             }
-
-            s.push(nums[i]);
-
-        }
