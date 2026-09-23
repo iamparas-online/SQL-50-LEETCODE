de9e896ff7e8fa15ebc584d4c402cfeb139f362a
Paras <new043one@gmail.com>
2026-09-23 11:33:40 +0530

    Add solution: Minimum Operations to Reduce X to Zero

--- minimum-operations-to-reduce-x-to-zero/solution.sql
+++ minimum-operations-to-reduce-x-to-zero/solution.sql
@@ -1,4 +1,13 @@
 
+
+        int left = 0;
+        int sum = 0;
+        int maxLen = -1;
+
+        for (int right = 0; right < nums.length; right++) {
+
+            sum += nums[right];
+
             while (sum > target && left <= right) {
                 sum -= nums[left];
                 left++;
@@ -11,8 +20,3 @@
 
         if (maxLen == -1) {
             return -1;
-        }
-
-        return nums.length - maxLen;
-    }
-}
