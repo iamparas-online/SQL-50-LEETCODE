c648b89be82fdcc1c1b2959965d24e3200a2f466
Paras <new043one@gmail.com>
2026-09-23 15:42:33 +0530

    Add solution: Count Subarrays With Majority Element I

--- count-subarrays-with-majority-element-i/solution.sql
+++ count-subarrays-with-majority-element-i/solution.sql
@@ -1,8 +1,9 @@
 
-class Solution {
-    public int countMajoritySubarrays(int[] nums, int target) {
-        int result=0;
-
-        for(int i=0; i<nums.length; i++){
-
             int count=0;
+
+            for(int j=i; <nums.length; j++){
+                int size=j-i+1;
+
+                if(nums[j]==target){
+                    count++;
+                }
