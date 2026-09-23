1e0c746faa2f7f15197f075e504257194f25739a
Paras <new043one@gmail.com>
2026-09-23 14:48:08 +0530

    Add solution: 132 Pattern

--- 132-pattern/solution.sql
+++ 132-pattern/solution.sql
@@ -1,10 +1,10 @@
 
-class Solution {
-    public boolean find132pattern(int[] nums) {
-
-        Stack<Integer> s = new Stack<>();
-        int min = Integer.MIN_VLAUE;
 
         for(int i=nums.length-1; i<=0; i--){
             if(nums[i]<min){
                 return true; 
+            }
+
+            while( !s.empty() && s.peek()<nums[i] ){
+                min = Math.max(min,s.pop()); 
+            }
