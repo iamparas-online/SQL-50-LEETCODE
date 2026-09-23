603e327ccaa7db5a3891f4b6a3f6b5198396d643
Paras <new043one@gmail.com>
2026-09-23 14:49:17 +0530

    Add solution: 132 Pattern

--- 132-pattern/solution.sql
+++ 132-pattern/solution.sql
@@ -1,11 +1,10 @@
 
-        Stack<Integer> s = new Stack<>();
-        int min = Integer.MIN_VLAUE;
-
-        for(int i=nums.length-1; i<=0; i--){
-            if(nums[i]<min){
-                return true; 
+                min = Math.max(min,s.pop()); 
             }
 
-            while( !s.empty() && s.peek()<nums[i] ){
-                min = Math.max(min,s.pop()); 
+            s.push(nums[i]);
+
+        
+        return false;
+        }
+}
