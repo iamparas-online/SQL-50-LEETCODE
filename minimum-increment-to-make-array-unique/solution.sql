a325eb064da45bac957ff2d9286b1c47390d7e9c
Paras <new043one@gmail.com>
2026-09-23 16:14:46 +0530

    Add solution: Minimum Increment to Make Array Unique

--- minimum-increment-to-make-array-unique/solution.sql
+++ minimum-increment-to-make-array-unique/solution.sql
@@ -5,4 +5,6 @@ class Solution {
         int ans=0;
 
         for(int i=1; i<nums.length; i++){
-            if(nums[i]<=nums[i-1]){}
+            if(nums[i]<=nums[i-1]){
+                i
+            }
