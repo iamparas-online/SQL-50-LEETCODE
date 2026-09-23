81417c7bf98eca06626af85e46587bc5f575a032
Paras <new043one@gmail.com>
2026-09-23 11:33:39 +0530

    Add solution: Minimum Operations to Reduce X to Zero

--- minimum-operations-to-reduce-x-to-zero/solution.sql
+++ minimum-operations-to-reduce-x-to-zero/solution.sql
@@ -1,1 +1,18 @@
 
+            while (sum > target && left <= right) {
+                sum -= nums[left];
+                left++;
+            }
+
+            if (sum == target) {
+                maxLen = Math.max(maxLen, right - left + 1);
+            }
+        }
+
+        if (maxLen == -1) {
+            return -1;
+        }
+
+        return nums.length - maxLen;
+    }
+}
