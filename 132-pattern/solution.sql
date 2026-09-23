3fa1d7f9778a46464ccda8189a7cc73a2d5e828d
Paras <new043one@gmail.com>
2026-09-23 14:28:24 +0530

    Add solution: 132 Pattern

--- 132-pattern/solution.sql
+++ 132-pattern/solution.sql
@@ -1,10 +1,10 @@
 
-class Solution {
-    public boolean find132pattern(int[] nums) {
+                if (minLeft < nums[k] && nums[k] < nums[j]) {
+                    return true;
+                }
+            }
 
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                for (int k = j + 1; k < nums.length; k++) {
+            minLeft = Math.min(minLeft, nums[j]);
+        }
 
-                    if (nums[i] < nums[k] && nums[k] < nums[j]) {
-                        return true;
+        return false;
