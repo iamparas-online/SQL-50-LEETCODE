e42ff37e698238b04900ac0100fcbc19f6b81187
Paras <new043one@gmail.com>
2026-09-23 14:51:55 +0530

    Add solution: 132 Pattern

--- 132-pattern/solution.sql
+++ 132-pattern/solution.sql
@@ -1,11 +1,11 @@
 
-        for(int i=nums.length-1; i<=0; i--){
-
-            if(nums[i]<min){
-                return true; 
             }
 
             while( !s.empty() && s.peek()<nums[i] ){
                 min = Math.max(min,s.pop()); 
 
             }
+
+            s.push(nums[i]);
+
+        }
