6f61305e2de7b1d5d92e06720afacb8bfb705cc2
Paras <new043one@gmail.com>
2026-09-23 11:28:35 +0530

    Add solution: Find X Value of Array II

--- find-x-value-of-array-ii/solution.sql
+++ find-x-value-of-array-ii/solution.sql
@@ -1,1 +1,16 @@
 
+
+class Solution {
+
+    int k;
+    Node[] tree;
+
+    class Node {
+        int product;
+        int[] pre;
+        int[] suf;
+
+        Node() {
+            pre = new int[k];
+            suf = new int[k];
+        }
