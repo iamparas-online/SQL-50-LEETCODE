b741cd653313fdf4466b4a82d240cb1e7be72b2b
Paras <new043one@gmail.com>
2026-09-23 14:53:36 +0530

    Add solution: 132 Pattern

--- 132-pattern/solution.sql
+++ 132-pattern/solution.sql
@@ -1,11 +1,11 @@
 
 class Solution {
-    public boolean find132pattern(int[] nums) {
+    public boolean find132pattern(int[] nums) 
+    {
 
         Stack<Integer> s = new Stack<>();
         int min = Integer.MIN_VALUE;
 
-        for(int i=nums.length-1; i=0; i--){
+        for(int i=nums.length-1; i>=0; i--){
 
             if(nums[i]<min){
-                return true; 
