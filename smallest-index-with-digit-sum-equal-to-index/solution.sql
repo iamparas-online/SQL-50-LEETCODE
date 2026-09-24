6b2c79382a48f08f2590632b35b0bb6b2505f47f
Paras <new043one@gmail.com>
2026-09-24 14:50:45 +0530

    Add solution: Smallest Index With Digit Sum Equal to Index

--- smallest-index-with-digit-sum-equal-to-index/solution.sql
+++ smallest-index-with-digit-sum-equal-to-index/solution.sql
@@ -1,17 +1,1 @@
 
-class Solution {
-    public int smallestIndex(int[] nums) {
-
-        for(int i = 0; i < nums.length; i++) {
-
-            int num = nums[i];
-            int sum = 0;
-
-            while(num>0){}
-
-            if(sum == i) {
-                return i;
-            }
-        }
-
-        return -1;
