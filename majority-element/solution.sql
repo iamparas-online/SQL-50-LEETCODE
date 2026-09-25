65127f9a7f57792298fac5723bb5ff370055847f
Paras <new043one@gmail.com>
2026-09-25 19:19:46 +0530

    Add solution: Majority Element

--- majority-element/solution.sql
+++ majority-element/solution.sql
@@ -1,4 +1,14 @@
 
+class Solution {
+    public int majorityElement(int[] nums) {
+
+        //create a HashMap to store key and value pair where key is 
+        element and value is how many times it comes basically values 
+        works as count 
+        HashMap<Integer,Integer> has = new HashMap<>();
+
+        //basic for loop
+        for(int i=0;i<nums.length;i++){
             
             //check element is present or not 
             //if present increase count using has.get() and has.put()
@@ -8,12 +18,3 @@
             else{
                 has.put(nums[i],1);
             }
-
-            //easiest part tbh has.get(nums[i]) checks the final count 
-            if(has.get(nums[i])>nums.length/2){
-                return nums[i];
-            }
-        }
-        return -1;
-    }
-}
