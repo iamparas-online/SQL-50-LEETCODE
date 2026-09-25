e477ffbcb4db5aa4aa2d96da01949d4c09d3d7f1
Paras <new043one@gmail.com>
2026-09-25 18:43:20 +0530

    Add solution: Majority Element

--- majority-element/solution.sql
+++ majority-element/solution.sql
@@ -6,7 +6,7 @@ class Solution {
         int count = 0;
 
         for(int i=0;i<nums.length;i++){
-            if(nums[i]==has.contain){
+            if(nums[i]==has.contain(i)){
                 count++;
             }
             else{
