4f81ef067111e510d67345723f96b63fc8476f5b
Paras <new043one@gmail.com>
2026-09-25 18:26:13 +0530

    Add solution: How Many Numbers Are Smaller Than the Current Number

--- how-many-numbers-are-smaller-than-the-current-number/solution.sql
+++ how-many-numbers-are-smaller-than-the-current-number/solution.sql
@@ -1,9 +1,4 @@
 
-    public int[] smallerNumbersThanCurrent(int[] nums) {
-        
-        //creating array 
-        int[] arr = new int[nums.length];
-
         //now loop 
         for(int i=0; i<nums.length; i++){
             //it resets to zero every time 
@@ -18,4 +13,3 @@
         }
         
     }
-}
