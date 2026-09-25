b653ea947d05628d50ae3d401e3a12d237d39e44
Paras <new043one@gmail.com>
2026-09-25 19:25:55 +0530

    Add solution: Majority Element

--- majority-element/solution.sql
+++ majority-element/solution.sql
@@ -1,10 +1,19 @@
 
-            //if present increase count using has.get() and has.put()
-            if(has.containsKey(nums[i])){
-                has.put(nums[i],has.get(nums[i])+1);
-            }
-            else{
-                has.put(nums[i],1);
-            }
+    public int majorityElement(int[] nums) {
 
-            //easiest part tbh has.get(nums[i]) checks the final 
+        HashMap<Integer,Integer> has = new HashMap<>();
+
+        for(int i=0; i<nums.length; i++){
+
+            // get current count; if element doesn't exist, use 0
+            has.put(nums[i], has.getOrDefault(nums[i], 0) + 1);
+
+            // check if current count is greater than half
+            if(has.get(nums[i]) > nums.length / 2){
+                return nums[i];
+            }
+        }
+
+        return -1;
+    }
+}
