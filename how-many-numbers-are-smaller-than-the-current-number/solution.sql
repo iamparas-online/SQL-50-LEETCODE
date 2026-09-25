72ce83bcc5f313ae35a97f2f8b8ac16d7d583e72
Paras <new043one@gmail.com>
2026-09-25 18:25:55 +0530

    Add solution: How Many Numbers Are Smaller Than the Current Number

--- how-many-numbers-are-smaller-than-the-current-number/solution.sql
+++ how-many-numbers-are-smaller-than-the-current-number/solution.sql
@@ -4,3 +4,17 @@ class Solution {
         
         //creating array 
         int[] arr = new int[nums.length];
+
+        //now loop 
+        for(int i=0; i<nums.length; i++){
+            //it resets to zero every time 
+            int count = 0;
+            for(int j=0; i<nums.length; i++){
+                if(nums[i]>nums[j]){
+                    count++;
+                }
+                arr[i] = count;
+            }
+        }
+        return arr;
+    }
