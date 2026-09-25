728d6df5b5514e87578c89cdb59c65edd1abc2da
Paras <new043one@gmail.com>
2026-09-25 18:28:25 +0530

    Add solution: How Many Numbers Are Smaller Than the Current Number

--- how-many-numbers-are-smaller-than-the-current-number/solution.sql
+++ how-many-numbers-are-smaller-than-the-current-number/solution.sql
@@ -13,8 +13,9 @@ class Solution {
                 if(nums[i]>nums[j]){
                     count++;
                 }
-                
+            
             }
+            arr[i] = count;
         }
         return arr;    
     }
