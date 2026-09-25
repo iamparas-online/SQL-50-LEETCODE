aaa2147c70b436f6ba36403f638a0fa1cbb1715b
Paras <new043one@gmail.com>
2026-09-25 18:35:16 +0530

    Add solution: Majority Element

--- majority-element/solution.sql
+++ majority-element/solution.sql
@@ -3,7 +3,6 @@ class Solution {
     public int majorityElement(int[] nums) {
         
         for(int i=0;i<nums.length;i++){
-            int count=;
+            int count=0;
         }
     }
-}
