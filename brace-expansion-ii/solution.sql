d061c76daf686e4c0f49a45be360bd8077693e6c
Paras <new043one@gmail.com>
2026-09-25 19:36:18 +0530

    Add solution: Brace Expansion II

--- brace-expansion-ii/solution.sql
+++ brace-expansion-ii/solution.sql
@@ -1,1 +1,22 @@
 
+class Solution {
+
+    int i = 0;
+
+    public List<String> braceExpansionII(String expression) {
+
+        Set<String> set = parse(expression);
+
+        List<String> ans = new ArrayList<>(set);
+        Collections.sort(ans);
+
+        return ans;
+    }
+
+    Set<String> parse(String s) {
+
+        Set<String> result = new HashSet<>();
+        Set<String> current = new HashSet<>();
+        current.add("");
+
+        while (i < s.length() && s.charAt(i) != '}') {
