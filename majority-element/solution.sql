353559e803b4d900dd9cf215f892a985b0898a31
Paras <new043one@gmail.com>
2026-09-25 19:13:51 +0530

    Add solution: Majority Element

--- majority-element/solution.sql
+++ majority-element/solution.sql
@@ -6,7 +6,7 @@ class Solution {
         is how many times it comes basically values works as count 
         HashMap<Integer,Integer> has = new HashMap<>();
 
-        //basic for l
+        //basic for loop
         for(int i=0;i<nums.length;i++){
             if(has.containsKey(nums[i])){
                 has.put(nums[i],has.get(nums[i])+1);
@@ -18,4 +18,3 @@ class Solution {
             if(has.get(nums[i])>nums.length/2){
                 return nums[i];
             }
-        }
