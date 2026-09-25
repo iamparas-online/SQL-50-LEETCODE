53f5b458a28c9444872c27864b3ce840c8c7ec1a
Paras <new043one@gmail.com>
2026-09-25 19:04:02 +0530

    Add solution: Majority Element

--- majority-element/solution.sql
+++ majority-element/solution.sql
@@ -1,1 +1,16 @@
 
+class Solution {
+    public int majorityElement(int[] nums) {
+
+        HashMap<Integer,Integer> has = new HashMap<>();
+
+        for(int i=0;i<nums.length;i++){
+            if(has.containsKey(nums[i])){
+                has.put(nums[i],has.get(nums[i]+1));
+            }
+            else{
+                has.put(has.get(nums[i]),nums[i]);
+            }
+
+            if(has.get(nums[i])>nums/2){
+                return has.get(nums[i]);
