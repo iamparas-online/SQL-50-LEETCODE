80dea1a7332c0d4a86be2e4a6726544a0e4da7a9
Paras <new043one@gmail.com>
2026-09-25 18:00:39 +0530

    Add solution: Brace Expansion II

--- brace-expansion-ii/solution.sql
+++ brace-expansion-ii/solution.sql
@@ -1,2 +1,21 @@
 
-c
+    Set<String> parse(String s) {
+
+        Set<String> result = new HashSet<>();
+        Set<String> current = new HashSet<>();
+        current.add("");
+
+        while (i < s.length() && s.charAt(i) != '}') {
+
+            char c = s.charAt(i);
+
+            if (c == '{') {
+
+                i++;
+                Set<String> inside = parse(s);
+                i++;
+
+                current = multiply(current, inside);
+
+            } 
+            else if (c == ',') {
