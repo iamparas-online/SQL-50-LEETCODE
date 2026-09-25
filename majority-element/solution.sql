b54277107712b4f398bdcbb78da77d5183c3be04
Paras <new043one@gmail.com>
2026-09-25 18:39:24 +0530

    Add solution: Majority Element

--- majority-element/solution.sql
+++ majority-element/solution.sql
@@ -3,8 +3,8 @@ class Solution {
     public int majorityElement(int[] nums) {
 
         Hashmap<Integer,Integer> has = new Hashmap<>()
-        int count = 0
+        int count = 0;
+        
         for(int i=0;i<nums.length;i++){
         }
     }
-}
