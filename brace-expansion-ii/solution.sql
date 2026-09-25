30e1f7d3dc145b558a1108d926865153d0db837f
Paras <new043one@gmail.com>
2026-09-25 19:36:24 +0530

    Add solution: Brace Expansion II

--- brace-expansion-ii/solution.sql
+++ brace-expansion-ii/solution.sql
@@ -1,23 +1,20 @@
 
 
-        Set<String> set = parse(expression);
+        result.addAll(current);
 
-        List<String> ans = new ArrayList<>(set);
-        Collections.sort(ans);
+        return result;
+    }
+
+    Set<String> multiply(Set<String> a, Set<String> b) {
+
+        Set<String> ans = new HashSet<>();
+
+        for (String x : a) {
+            for (String y : b) {
+                ans.add(x + y);
+            }
+        }
 
         return ans;
     }
-
-    Set<String> parse(String s) {
-
-        Set<String> result = new HashSet<>();
-        Set<String> current = new HashSet<>();
-        current.add("");
-
-        while (i < s.length() && s.charAt(i) != '}') {
-
-            char c = s.charAt(i);
-
-            if (c == '{') {
-
-                i++;
+}
