b022e6dcf2d69fc636e6463aaf63483831668506
Paras <new043one@gmail.com>
2026-09-25 19:13:05 +0530

    Add solution: Majority Element

--- majority-element/solution.sql
+++ majority-element/solution.sql
@@ -3,7 +3,7 @@ class Solution {
     public int majorityElement(int[] nums) {
 
         //create a HashMap to store key and value pair where key is element and value 
-        is how many
+        is how many times 
         HashMap<Integer,Integer> has = new HashMap<>();
 
         for(int i=0;i<nums.length;i++){
