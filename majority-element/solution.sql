99fb574e276d8d189d9205b8b0cdc2682ea7c3a6
Paras <new043one@gmail.com>
2026-09-25 19:13:17 +0530

    Add solution: Majority Element

--- majority-element/solution.sql
+++ majority-element/solution.sql
@@ -3,7 +3,7 @@ class Solution {
     public int majorityElement(int[] nums) {
 
         //create a HashMap to store key and value pair where key is element and value 
-        is how many times it comes basica
+        is how many times it comes basically v
         HashMap<Integer,Integer> has = new HashMap<>();
 
         for(int i=0;i<nums.length;i++){
