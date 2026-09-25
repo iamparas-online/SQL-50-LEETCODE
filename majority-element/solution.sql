2b9ba55d4c876a2c601b0c8cb5a97a9631cc1695
Paras <new043one@gmail.com>
2026-09-25 19:19:43 +0530

    Add solution: Majority Element

--- majority-element/solution.sql
+++ majority-element/solution.sql
@@ -1,4 +1,5 @@
 
+            
             //check element is present or not 
             //if present increase count using has.get() and has.put()
             if(has.containsKey(nums[i])){
@@ -8,8 +9,7 @@
                 has.put(nums[i],1);
             }
 
-            //easiest part tbh has.get(nums[i]) checks the final 
-            count 
+            //easiest part tbh has.get(nums[i]) checks the final count 
             if(has.get(nums[i])>nums.length/2){
                 return nums[i];
             }
