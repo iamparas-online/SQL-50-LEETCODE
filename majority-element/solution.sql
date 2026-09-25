8e859d0f545d30100ab374d004ecd2e8e8a609d0
Paras <new043one@gmail.com>
2026-09-25 18:46:58 +0530

    Add solution: Majority Element

--- majority-element/solution.sql
+++ majority-element/solution.sql
@@ -1,10 +1,10 @@
 
 
         HashMap<Integer,Integer> has = new HashMap<>();
-        int count = 0;
+        int count = 0
 
         for(int i=0;i<nums.length;i++){
-            if(has.containsKey(ni)){
+            if(has.containsKey(nums(i)){
                 count++;
             }
             else{
