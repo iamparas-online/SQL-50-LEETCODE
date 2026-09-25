4320bd592b8bf1167a2c101595e6ee3f654822cc
Paras <new043one@gmail.com>
2026-09-25 18:43:27 +0530

    Add solution: Majority Element

--- majority-element/solution.sql
+++ majority-element/solution.sql
@@ -6,7 +6,7 @@ class Solution {
         int count = 0;
 
         for(int i=0;i<nums.length;i++){
-            if(nums[i]==has.contain(i)){
+            if(nums[i]==has.containK(i)){
                 count++;
             }
             else{
