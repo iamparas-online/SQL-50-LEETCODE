5b849d1469c0e7381205d6ac4eee07e946bb7ebf
Paras <new043one@gmail.com>
2026-09-25 18:53:42 +0530

    Add solution: Majority Element

--- majority-element/solution.sql
+++ majority-element/solution.sql
@@ -6,7 +6,7 @@ class Solution {
 
         for(int i=0;i<nums.length;i++){
             if(has.containsKey(nums[i]){
-                has.put();
+                has.put(i);
             }
             else{
                 has.put(has.get(),nums[i]);
