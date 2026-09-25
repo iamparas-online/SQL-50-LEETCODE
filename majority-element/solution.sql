97940e11338d9b60301c6526b151627e4253530c
Paras <new043one@gmail.com>
2026-09-25 19:10:46 +0530

    Add solution: Majority Element

--- majority-element/solution.sql
+++ majority-element/solution.sql
@@ -8,13 +8,3 @@
                 has.put(nums[i],has.get(nums[i])+1);
             }
             else{
-                has.put(nums[i],1);
-            }
-
-            if(has.get(nums[i])>nums.length/2){
-                return nums[i];
-            }
-        }
-        return -1;
-    }
-}
