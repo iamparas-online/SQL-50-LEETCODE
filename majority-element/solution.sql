4b07e6e8090d6ed4d0f452db906fc28efb81d00b
Paras <new043one@gmail.com>
2026-09-25 18:52:22 +0530

    Add solution: Majority Element

--- majority-element/solution.sql
+++ majority-element/solution.sql
@@ -9,7 +9,7 @@ class Solution {
                 count++;
             }
             else{
-                has.put(nums[i],);
+                has.put(nums[i,);
             }
 
             if(count>nums/2){
