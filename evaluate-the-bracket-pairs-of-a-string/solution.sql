a471e7f01c0c5904a36166da4a97d274703e9f7c
Paras <new043one@gmail.com>
2026-09-26 09:53:16 +0530

    Add solution: Evaluate the Bracket Pairs of a String

--- evaluate-the-bracket-pairs-of-a-string/solution.sql
+++ evaluate-the-bracket-pairs-of-a-string/solution.sql
@@ -8,7 +8,7 @@ class Solution {
         if(s.charAt(i)=='('){
             i++;
             while(s.charAt(i) != ')'){
-                SB.append(charAt(i))
+                SB.append(charAt(i));
                 i++;
             }
         }
