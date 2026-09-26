82301fe4923b52eff37bed245238b51fa2e0c551
Paras <new043one@gmail.com>
2026-09-26 09:25:39 +0530

    Add solution: Evaluate the Bracket Pairs of a String

--- evaluate-the-bracket-pairs-of-a-string/solution.sql
+++ evaluate-the-bracket-pairs-of-a-string/solution.sql
@@ -2,7 +2,7 @@
 class Solution {
     public String evaluate(String s, List<List<String>> knowledge) {
         HashMap<String,String> has = new HashMap<>();
-        StringBuilder str = new StringBuilder()<>;
+        StringBuilder str = new StringBuilder()<>();
 
     }
 }
