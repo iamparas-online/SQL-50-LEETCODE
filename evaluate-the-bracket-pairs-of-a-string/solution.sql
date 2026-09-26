3b085c881b4328a26ff602eab207c2fd3a4d133c
Paras <new043one@gmail.com>
2026-09-26 10:11:39 +0530

    Add solution: Evaluate the Bracket Pairs of a String

--- evaluate-the-bracket-pairs-of-a-string/solution.sql
+++ evaluate-the-bracket-pairs-of-a-string/solution.sql
@@ -14,7 +14,7 @@ class Solution {
         }
         for(int i=0; i<knowledge.length; i++){
         knowledge.get(0).get(0);
-        knowledge.get(0).get(1);
+        knowledge.get(0).get(1);}
 
         }
 
