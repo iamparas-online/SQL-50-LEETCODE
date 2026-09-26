629065a70a22785ae38eb2fd272aa2645054c28b
Paras <new043one@gmail.com>
2026-09-26 09:27:16 +0530

    Add solution: Evaluate the Bracket Pairs of a String

--- evaluate-the-bracket-pairs-of-a-string/solution.sql
+++ evaluate-the-bracket-pairs-of-a-string/solution.sql
@@ -4,7 +4,7 @@ class Solution {
         HashMap<String,String> has = new HashMap<>();
         StringBuilder str = new StringBuilder<>();
 
-        for(int i=0; i<s.len)
+        for(int i=0; i<s.length;)
         if(has.charAt=='('){
             str.append(i);
         }
